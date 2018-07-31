677a9a47830efde685ab4c34c66645a2b6ef32e0
DESKTOP-47RMOSE\Ben <benwalker91@live.com.au>
2018-07-31 21:44:13 +1000

    Added, obstacle collision and endstate

---
+++ New Unity Project/Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    public PlayerMovement pM;
+    public Camera mainCam;
+    public Canvas endScreen;
+
+	void Start ()
+    {
+        pM = FindObjectOfType<PlayerMovement>();
+        mainCam = FindObjectOfType<Camera>();
+        endScreen = FindObjectOfType<Canvas>();
+    }
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            pM.sS.Health--;
+            if (pM.sS.Health <= 0)
+            {
+                Destroy(col.gameObject.GetComponentInChildren<Player>());
+                Destroy(col.gameObject.GetComponentInChildren<PlayerMovement>());
+                mainCam.transform.parent = null;
+                StartCoroutine("TimeTillEnd");
+                LeanTween.move(mainCam.gameObject, new Vector3(transform.position.x - 1, transform.position.y, transform.position.z - 15), 3);
+            }
+        }
+    }
+    
+    public IEnumerator TimeTillEnd()
+    {
+        yield return new WaitForSeconds(3);
+        Time.timeScale = 0;
+        endScreen.transform.GetChild(0).gameObject.SetActive(true);
+        StopCoroutine("TimeTillEnd");
+    }
+}
