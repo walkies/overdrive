2844214a45a460a12f30fc0fbdce86cb2f98d2c4
DESKTOP-47RMOSE\Ben <benwalker91@live.com.au>
2018-08-11 16:29:25 +1000

    Carmovement

---
+++ New Unity Project/Assets/Scripts/CarsMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarsMovement : MonoBehaviour
+{
+
+	void Update ()
+    {
+        transform.position -= transform.forward * (Time.deltaTime * 3); 
+	}
+}
